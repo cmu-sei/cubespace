5e2ecf24f48a91ed0b8f04a7097aa894b4821692
Scott King <jsking@andrew.cmu.edu>
2024-02-13 11:33:52 -0500

    Tooltip testing

--- Assets/Scripts/UI/HUD/UIHudGalaxyPanelManager.cs
+++ Assets/Scripts/UI/HUD/UIHudGalaxyPanelManager.cs
@@ -42,7 +42,7 @@ namespace UI.HUD
             // These should be disabled by their controllers but if there's no missions their controllers aren't instantiated and the tooltips stay open.
             // These calls will be redundant in other cases but will prevent that problem
             if (DisplayTooltip.Instance != null) DisplayTooltip.Instance.gameObject.SetActive(false);
-            if (SolveCountTooltip.Instance != null) SolveCountTooltip.Instance.gameObject.SetActive(false);
+            //if (SolveCountTooltip.Instance != null) SolveCountTooltip.Instance.gameObject.SetActive(false);
             if (PointsTooltip.Instance != null) PointsTooltip.Instance.gameObject.SetActive(false);
         }
 
